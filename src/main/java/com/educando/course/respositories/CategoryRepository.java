9857bf9357b719396fa575647e2156ccb7de8a8f
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-05-02 22:46:55 -0300

    Category (Categoria)

---
+++ src/main/java/com/educando/course/respositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.educando.course.respositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educando.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
---
+++ src/main/java/com/educando/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.educando.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educando.course.entities.Category;
+import com.educando.course.respositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
